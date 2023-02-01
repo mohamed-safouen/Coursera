7b1512c67d66a544a1590f7b08c755728fc005ce
mohamed-safouen <mohamedsafouen@gmail.com>
2023-02-01 09:52:44 +0100

    (web/Little Lemon Restaurant):test Change router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import {HashRouter } from "react-router-dom";
 
 import {Router} from './router/router';
 import reportWebVitals from './reportWebVitals';
@@ -11,9 +11,9 @@ import "./style.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter >
+    <HashRouter >
       <Router />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
